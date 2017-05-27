07bd09351e6386b7fd8ee6c1c4bfab5a82122747
SYM <SYM@SYM.local>
2017-05-27 15:32:07 +0800

    细节调整，关闭vue调试模式

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,6 +8,11 @@ let Vue = require('../../node_modules/vue/dist/vue');
 let auitab = require('../../public/lib/aui-tab');
 let component = require('../../components/componentsEntry');
 
+const isDebug_mode = process.env.NODE_ENV !== 'production';
+Vue.config.debug = isDebug_mode;
+Vue.config.devtools = isDebug_mode;
+Vue.config.productionTip = isDebug_mode;
+
 let app = new Vue({
     el: "#app",
     data: {
