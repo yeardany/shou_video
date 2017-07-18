52050bf3430119bd7110d10cc547bb6760a7b6b6
SYM <SYM@SYM.local>
2017-07-18 11:37:42 +0800

    整合leancloud，准备后端重构

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var timeout = require('connect-timeout');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var AV = require('leanengine');
+// var favicon = require('serve-favicon');
+
+AV.init({
+    appId: process.env.LEANCLOUD_APP_ID || 'OEKSm7LJElxeU640s5nkYJ1j-gzGzoHsz',
+    appKey: process.env.LEANCLOUD_APP_KEY || 'NCEMV8RnquFsSxd4LTFRRkmi',
+    masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'RQ5iasqaIaEwBbQS1gjPo7OJ'
+});
+
+// 如果不希望使用 masterKey 权限，可以将下面一行删除
+AV.Cloud.useMasterKey();
 
 var mongoose = require('mongoose');
 var MongoDB_Conf = require('./config');
@@ -16,24 +27,21 @@ var users = require('./routes/users_routes');
 var app = express();
 mongoose.connect(MongoDB_Conf.database, {useMongoClient: true});
 
-// view engine setup
+//设置模板
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//加载中间件
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-// app.use(require('node-sass-middleware')({
-//     src: path.join(__dirname, 'public'),
-//     dest: path.join(__dirname, 'public'),
-//     indentedSyntax: true,
-//     sourceMap: true
-// }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(timeout('15s'));// 设置默认超时时间
+app.use(AV.express());
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
+//设置路由
 app.use('/', index);
 app.use('/videos', videos);
 app.use('/categories', categories);
